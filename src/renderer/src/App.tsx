5b448ef1687d1bbaa0ee93cdfd2b89299b30f7e4
Michael Berezovsky <mishka.ber@gmail.com>
2024-07-22 00:59:05 +0300

    init

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,25 @@
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+
+function App(): JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <img alt="logo" className="logo" src={electronLogo} />
+      <div className="creator">Developed by Michael Berezovsky</div>
+      <div className="text">
+        Download <span className="react">QuickLook</span>
+      </div>
+      <p className="tip">
+        Please try pressing <code>F12</code> to open the devTool
+      </p>
+      <button className="btn" onClick={ipcHandle}>
+        Ping
+      </button>
+      <Versions />
+    </>
+  )
+}
+
+export default App
